98dbfc3f1a86af1628279661252136f31371bedb
codeWithFatema <fatema14384@gmail.com>
2024-04-22 21:29:13 +0300

    inital commit

--- js/section1/index.js
+++ js/section1/index.js
@@ -1,6 +1,26 @@
 //node index.js
 
 //variables
-let name;
+let name = "node";
 console.log(name);
 console.log("Hello World!");
+
+//constant variables
+const Name = "constant";
+Name = "hello";
+console.log(Name);
+
+let age = 17;
+if (age < 5) {
+  console.log("Baby");
+} else if (age >= 5 && age <= 12) {
+  console.log("Child");
+} else if (age > 12 && age <= 18) {
+  console.log("Teenager");
+} else {
+  console.log("Adult");
+}
+
+//data types
+let x = 34;
+console.log(`The value of x is ${x}`);
