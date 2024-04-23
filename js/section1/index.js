5cf814cca7e8ec794bf1247c85456254cf3bf5eb
codeWithFatema <fatema14384@gmail.com>
2024-04-23 22:09:07 +0300

    initial commit

--- js/section1/index.js
+++ js/section1/index.js
@@ -10,17 +10,30 @@ const Name = "constant";
 Name = "hello";
 console.log(Name);
 
-let age = 17;
-if (age < 5) {
-  console.log("Baby");
-} else if (age >= 5 && age <= 12) {
-  console.log("Child");
-} else if (age > 12 && age <= 18) {
-  console.log("Teenager");
-} else {
-  console.log("Adult");
-}
+// let age = 17;
+// if (age < 5) {
+//   console.log("Baby");
+// } else if (age >= 5 && age <= 12) {
+//   console.log("Child");
+// } else if (age > 12 && age <= 18) {
+//   console.log("Teenager");
+// } else {
+//   console.log("Adult");
+// }
 
 //data types
 let x = 34;
 console.log(`The value of x is ${x}`);
+
+//static type and dynamic type
+let str = "I am a string.";
+console.log(typeof str); //output:string
+str = 34;
+console.log(typeof str); //output:number
+
+//arrays
+// let arr = [1, 2, 3];
+// console.log(arr[0]); //output:1
+// console.log(arr[1]); //output:2
+
+//objects
